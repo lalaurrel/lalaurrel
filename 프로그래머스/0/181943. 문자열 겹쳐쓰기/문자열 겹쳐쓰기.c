513c76b4d76ac526bdefdbfdb213009f600283b8
Ajin Park <108174693+lalaurrel@users.noreply.github.com>
2025-02-13 03:26:22 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+char* solution(const char* my_string, const char* overwrite_string, int s) {
+   int len = strlen(my_string);
+   char* answer = (char*)malloc(len + 1);
+   strcpy(answer, my_string);
+   
+   for(int i = 0; i < strlen(overwrite_string); i++) {
+       answer[s + i] = overwrite_string[i];
+   }
+   
+   return answer;
+}
